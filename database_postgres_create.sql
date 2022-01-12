283e686af38addeb186dc5bef6c6d321e107128f
Vince <vincech@gmail.com>
2022-01-11 21:49:40 -0500

    create tables

--- database_postgres_create.sql
+++ database_postgres_create.sql
@@ -16,3 +16,17 @@ CREATE TABLE IF NOT EXISTS workout_card (
     "athlete_id" serial REFERENCES athletes("_id"),
     CONSTRAINT "workout_card_id_pk" PRIMARY KEY ("_id")
 );
+
+CREATE TABLE IF NOT EXISTS subscription (
+    "_id" serial NOT NULL,
+    "athlete_id" serial REFERENCES athletes("_id") NOT NULL,
+    "following" serial REFERENCES athletes("_id") NOT NULL,
+    CONSTRAINT "subscription_id_pk" PRIMARY KEY ("_id")
+);
+
+CREATE TABLE IF NOT EXISTS tag (
+    "_id" serial NOT NULL,
+    "workout_id" serial REFERENCES workout_card("_id") NOT NULL,
+    "athlete_id" serial REfERENCES athletes("_id") NOT NULL,
+    CONSTRAINT "tag_id_pk" PRIMARY KEY ("_id")
+);
